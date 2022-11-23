f2d95dd9596b35b275715ec27676059b840f37c1
Cliff Ndukuya <84091410+Cliffndukuya@users.noreply.github.com>
2022-11-23 12:29:55 +0200

    Components creation

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserHomeComponent } from './components/user-home/user-home.component';
+import { AdminHomeComponent } from './components/admin-home/admin-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    RegisterComponent,
+    LoginComponent,
+    UserHomeComponent,
+    AdminHomeComponent
   ],
   imports: [
     BrowserModule,
