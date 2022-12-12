9c09edf534e9895198eade157f02737ca9caa3c5
Cliff Ndukuya <84091410+Cliffndukuya@users.noreply.github.com>
2022-12-12 14:43:20 +0200

    Update profile.component.ts

--- Frontend/src/app/components/profile/profile.component.ts
+++ Frontend/src/app/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-profile',
@@ -11,5 +14,19 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  pInfoForm = new FormGroup({
+    user_name: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z ]*$')]),
+    user_lastname: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z ]*$')]),
+    password: new FormControl(),
+    confimPassword: new FormControl(),
+  });
+
+  shipInfoForm = new FormGroup({
+    address1: new FormControl(''),
+    address2: new FormControl(''),
+    city: new FormControl(''),
+    zipCode: new FormControl(),
+  });
 
 }
