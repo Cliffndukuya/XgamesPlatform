e0a53446c29ff7cbc0c675ef32fbeb3bfd459bf9
Cliff Ndukuya <84091410+Cliffndukuya@users.noreply.github.com>
2022-12-12 14:43:35 +0200

    Update app-routing.module.ts

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { AdminHomeComponent } from './components/admin-home/admin-home.component';
 import { UserHomeComponent } from './components/user-home/user-home.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
 
@@ -13,7 +14,8 @@ const routes: Routes = [
 {path: 'login', component: LoginComponent},
 {path: 'admin', component: AdminHomeComponent},
 {path: 'user', component: UserHomeComponent},
-{path: 'register', component: RegisterComponent}
+{path: 'register', component: RegisterComponent},
+{path: 'profile', component: ProfileComponent}
 
 ];
 
